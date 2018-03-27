5354913cbb53d8d8f1e72f51183428a60e3ece78
BINARYUPDATES.COM <binaryupdates@gmail.com>
2018-03-27 23:21:28 +0530

    Add files via upload

---
+++ 1_arduino_lcd_printonlcd.ino
@@ -0,0 +1,26 @@
+#include <LiquidCrystal.h>
+
+const int RS = 12;
+const int EN = 11;
+const int D5 = 5;
+const int D4 = 4;
+const int D3 = 3;
+const int D2 = 2;
+
+LiquidCrystal lcd(RS, EN, D5, D4, D3, D2);
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("HELLO");
+}
+
+void loop() {
+
+}
+
+
+
+
+
+
+
---
+++ 5_arduino_lcd_scroll_effect.ino
@@ -0,0 +1,31 @@
+#include <LiquidCrystal.h>
+
+const int RS = 12;
+const int EN = 11;
+const int D5 = 5;
+const int D4 = 4;
+const int D3 = 3;
+const int D2 = 2;
+
+int count = 0;
+
+LiquidCrystal lcd(RS, EN, D5, D4, D3, D2);
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("HELLO");
+}
+
+void loop() {
+  for (int i = 0; i < 16; i++) {
+    lcd.scrollDisplayRight();
+    delay(250);
+  }
+}
+
+
+
+
+
+
+
